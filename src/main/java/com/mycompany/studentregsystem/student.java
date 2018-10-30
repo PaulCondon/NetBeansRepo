2cdd07a9d3f38f6484beefcdcbd7011f6e5fdb00
Paul Condon <paul.condon96@gmail.com>
2018-10-30 20:22:50 +0000

    first commit

---
+++ src/main/java/com/mycompany/studentregsystem/student.java
@@ -0,0 +1,81 @@
+package com.mycompany.studentregsystem;
+
+import java.util.ArrayList;
+
+/*A student class should contain some attributes such as 
+ Name, Age, DOB, ID, username, courses and modules registered for, etc.).
+A specific method (getUsername()) will generate the student’s username by
+concatenating their name and age.
+ */
+public class student {
+
+    public String name;
+    public int age;
+    public String DOB;
+    public long ID;
+    public String username;
+    public ArrayList regCourses;
+    public ArrayList regModules;
+
+    public student(String name, int age, String DOB, long ID, ArrayList regCourses, ArrayList regModules) {
+        this.name = name;
+        this.age = age;
+        this.DOB = DOB;
+        this.ID = ID;
+        this.regCourses = regCourses;
+        this.regModules = regModules;
+    }
+
+    public String getUsername() {
+        username = name + this.age;
+        return username;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getDOB() {
+        return DOB;
+    }
+
+    public void setDOB(String dOB) {
+        DOB = dOB;
+    }
+
+    public long getID() {
+        return ID;
+    }
+
+    public void setID(long iD) {
+        ID = iD;
+    }
+
+    public ArrayList getRegCourses() {
+        return regCourses;
+    }
+
+    public void setRegCourses(ArrayList regCourses) {
+        this.regCourses = regCourses;
+    }
+
+    public ArrayList getRegModules() {
+        return regModules;
+    }
+
+    public void setRegModules(ArrayList regModules) {
+        this.regModules = regModules;
+    }
+}
---
+++ src/test/java/StudentRegTest.java
@@ -0,0 +1,58 @@
+import com.mycompany.studentregsystem.student;
+import java.util.ArrayList;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Paul
+ * Write a simple unit test using JUnit to 
+ * make sure that all the above works (e.g. get getUsername()) as expected.
+ */
+public class StudentRegTest {
+    
+    public static student x;
+    public ArrayList<String> regcourse;
+    public ArrayList<String> regmodules;
+    
+    @BeforeClass
+    public static void setUpClass() {
+        
+        ArrayList<String> regcourse = new ArrayList<String>();
+        ArrayList<String> regmodules  = new ArrayList<String>();
+        
+        
+       // x = new student("Tim", 20, "20/09/1996", 15350786, course, modules);
+        
+    }
+    @Test
+    public void Test() {
+
+        x = new student("Tim", 20, "20/09/1996", 15350786, regcourse, regmodules);
+        //x = new student();
+
+        String username = x.getUsername();
+        assertEquals("Tim20", username);
+    }
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+}
